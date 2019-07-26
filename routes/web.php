a36e7a254cfaea21b717b38e63533f0c2629a34d
Valentin <valentin@waldeck.dev>
2019-07-26 13:33:05 +0200

    Enabled built-in auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+
+Auth::routes();
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
+Route::get('/home', 'HomeController@index')->name('home');
